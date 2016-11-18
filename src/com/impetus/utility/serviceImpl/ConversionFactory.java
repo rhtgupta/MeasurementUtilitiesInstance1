5faab4c7899868fa39359404f2b02b87949c189d
ajay <ajay.tushir04@gmail.com>
2016-11-18 16:44:17 +0530

    ok

--- src/com/impetus/utility/serviceImpl/ConversionFactory.java
+++ src/com/impetus/utility/serviceImpl/ConversionFactory.java
@@ -1,7 +1,6 @@
 package com.impetus.utility.serviceImpl;
 
 import static com.impetus.utility.common.Constants.CONVERSION_TYPE_MEASUREMENT;
-import static com.impetus.utility.common.Constants.CONVERSION_TYPE_TEMPERATURE;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,18 +14,13 @@ public class ConversionFactory {
 
 	@Autowired
 	private MeasurmentConversionImpl measurmentConversionImpl;
-	@Autowired
-	private TemperatureConversionImpl temperatureConversionImpl;
-	
+
 	private Logger logger = LoggerFactory.getLogger(ConversionFactory.class);
 
 	public ConversionAbstract getConversionType(String conversionType) {
 		if (CONVERSION_TYPE_MEASUREMENT.equals(conversionType)) {
 			logger.info("Returning Measurment implementation");
 			return measurmentConversionImpl;
-		} else if (CONVERSION_TYPE_TEMPERATURE.equals(conversionType)) {
-			logger.info("Returning Temprature implementation");
-			return temperatureConversionImpl;
 		}
 		return null;
 	}
